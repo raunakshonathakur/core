e53829110ffc38d04ae864c6e75391692ac1b67b
Raunak Thakur <raunakthakur3047@gmai..com>
2016-03-22 18:33:21 -0400

    Made changes in UI and PHotoAlbumDialogue. Commented the Keep Aspect Ratio function in the UI and added a function called Fill slide

--- sd/source/ui/dlg/PhotoAlbumDialog.cxx
+++ sd/source/ui/dlg/PhotoAlbumDialog.cxx
@@ -59,7 +59,8 @@ SdPhotoAlbumDialog::SdPhotoAlbumDialog(vcl::Window* pWindow, SdDrawDocument* pAc
     get(pImg, "preview_img");
 
     get(pInsTypeCombo, "opt_combo");
-    get(pASRCheck, "asr_check");
+    get(pFILLCheck, "fill_check");
+    //get(pASRCheck, "asr_check");
     get(pCapCheck, "cap_check");
     pCancelBtn->SetClickHdl(LINK(this, SdPhotoAlbumDialog, CancelHdl));
     pCreateBtn->SetClickHdl(LINK(this, SdPhotoAlbumDialog, CreateHdl));
@@ -95,7 +96,8 @@ void SdPhotoAlbumDialog::dispose()
     pImagesLst.clear();
     pImg.clear();
     pInsTypeCombo.clear();
-    pASRCheck.clear();
+    pFILLCheck.clear();
+    //pASRCheck.clear();
     pCapCheck.clear();
     ModalDialog::dispose();
 }
@@ -156,11 +158,16 @@ IMPL_LINK_NOARG_TYPED(SdPhotoAlbumDialog, CreateHdl, Button*, void)
 
                 ::awt::Point aPicPos;
 
-                if(pASRCheck->IsChecked())
+                if (pFILLCheck->IsChecked())
+                {
+                    aPicSize = createFILLSize(aPicSize, aPageSize);
+                    pASRCheck->Disable();
+                }
+                /*else if(pASRCheck->IsChecked())
                 {
                     // Resize the image, with keeping ASR
                     aPicSize = createASRSize(aPicSize, aPageSize);
-                }
+                }*/
                 else
                 {
                     aPicSize.Width = aPageSize.Width;
@@ -671,6 +678,55 @@ awt::Size SdPhotoAlbumDialog::createASRSize(const awt::Size& aPicSize, const awt
         resizeWidth = resizeHeight * aspect;
     }
     return awt::Size(resizeWidth, resizeHeight);
+}
+awt::Size SdPhotoAlbumDialog::createFILLSize(const awt::Size& aPicSize, const awt::Size& aMaxSize)
+{
+    double picWidth = aPicSize.Width;
+    double picHeight = aPicSize.Height;
+    double aspect = picWidth/picHeight;
+    double sliWidth = aMaxSize.Width;
+    double sliHeight = aMaxSize.Height;
+    std::ofstream file;
+    int diffW = 0;
+    int diffH = 0;
+
+    if ((picWidth != sliWidth) || (picHeight != sliHeight))
+    {
+      diffW = sliWidth - picWidth;
+      diffH = sliHeight - picHeight;
+
+      if ((diffW > 0) || (diffH > 0))
+      {
+
+        if (diffW > diffH)
+        {
+
+          picWidth = sliWidth;
+          picHeight = picWidth/aspect;
+        }
+        else
+        {
+
+          picHeight = sliHeight;
+          picWidth = picHeight * aspect;
+        }
+      }
+      if ((diffW < 0) && (diffH < 0))
+      {
+        if (abs(diffW) < abs(diffH))
+        {
+
+          picWidth = sliWidth;
+          picHeight = picWidth/aspect;
+        }
+        else{
+
+          picHeight = sliHeight;
+          picWidth = picHeight * aspect;
+        }
+      }
+    }
+    return awt::Size(picWidth, picHeight);
 }
 
 void SdPhotoAlbumDialog::createCaption(const awt::Size& aPageSize )
